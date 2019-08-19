4156107415fd388c2ac72358da71f799f40efcd5
s1360841 <s1360841@korenvliet.local>
2019-08-20 00:13:00 +0200

    update ResNet

--- resnet.py
+++ resnet.py
@@ -4,21 +4,22 @@ import argparse
 
 from keras import backend as K
 from keras import callbacks
+from keras.optimizers import RMSprop
 from keras.models import Model
 from keras.layers import Conv2D, MaxPooling2D, AveragePooling2D, Input, Dense, Flatten, Activation, BatchNormalization, Add
-#from keras.utils import multi_gpu_model
+from keras.utils import multi_gpu_model
 
 from data_generator import AFLWFaceRegionsSequence
 from utils import plot_log
 
 def Conv1x1(filters, strides, name, inputs):
-    Conv2D(filters=filters, kernel_size=(1, 1), strides=strides, padding='valid', use_bias=False, name=name)(inputs)
+    return Conv2D(filters=filters, kernel_size=(1, 1), strides=strides, padding='same', use_bias=False, name=name)(inputs)
 
 def Conv3x3(filters, strides, name, inputs):
-    Conv2D(filters=filters, kernel_size=(3, 3), strides=strides, padding='valid', use_bias=False, name=name)(inputs)
+    return Conv2D(filters=filters, kernel_size=(3, 3), strides=strides, padding='same', use_bias=False, name=name)(inputs)
 
 def Conv7x7(filters, strides, name, inputs):
-    Conv2D(filters=filters, kernel_size=(7, 7), strides=strides, padding='valid', use_bias=False, name=name)(inputs)
+    return Conv2D(filters=filters, kernel_size=(7, 7), strides=strides, padding='same', use_bias=False, name=name)(inputs)
 
 def Conv1x1BatchNormReLU(filters, strides, name, inputs):
     return Activation(activation='relu')(BatchNormalization()(Conv1x1(filters, strides, name, inputs)))
@@ -29,79 +30,82 @@ def Conv3x3BatchNormReLU(filters, strides, name, inputs):
 def Conv7x7BatchNormReLU(filters, strides, name, inputs):
     return Activation(activation='relu')(BatchNormalization()(Conv7x7(filters, strides, name, inputs)))
 
-def Bottleneck(med_filters, out_filters, inputs, downsampling=False):
-    strides = (1, 1)
+def Bottleneck(med_filters, out_filters, name, inputs, downsampling=False):
     if(downsampling):
         strides = (2, 2)
+        b_identity  = BatchNormalization()(Conv3x3(out_filters, strides, name + '_bi', inputs))
+    else:
+        strides = (1, 1)
+        b_identity  = BatchNormalization()(Conv1x1(out_filters, strides, name + '_bi', inputs))
 
-    b_identity  = BatchNormalization()(Conv1x1(out_filters, (1, 1), 'b_identity', inputs))
-    b_conv1     = Conv1x1BatchNormReLU(med_filters, strides, 'b_conv1', inputs)
-    b_conv2     = Conv3x3BatchNormReLU(med_filters, (1, 1), 'b_conv2', b_conv1)
-    b_conv3     = Conv1x1(out_filters, (1, 1), 'b_conv3', b_conv2)
+    b_conv1     = Conv1x1BatchNormReLU(med_filters, (1, 1), name + '_b1', inputs)
+    b_conv2     = Conv3x3BatchNormReLU(med_filters, strides, name + '_b2', b_conv1)
+    b_conv3     = Conv1x1(out_filters, (1, 1), name + '_b3', b_conv2)
     b_bn3       = BatchNormalization()(b_conv3)
-    b_out       = Activation(activation='relu')(b_bn3 + b_identity)
+    b_add	= Add()([b_bn3, b_identity])
+    b_out       = Activation(activation='relu')(b_add)
 
     return b_out
 
 def ResNet101(input_shape):
     input_layer = Input(input_shape, name='input_tensor')
 
-    conv1       = Conv7x7BatchNormReLU(64, (2, 2), 'valid', 'conv1', input_layer)
-    maxpool     = MaxPooling2D(pool_size=(3,3), strides=(2, 2), padding='valid')(conv1)
+    conv1       = Conv7x7BatchNormReLU(64, (2, 2), 'conv1', input_layer)
+    maxpool     = MaxPooling2D(pool_size=(3,3), strides=(2, 2), padding='same')(conv1)
 
-    conv2_1     = Bottleneck(64, 256, maxpool)
-    conv2_2     = Bottleneck(64, 256, conv2_1)
-    conv2_3     = Bottleneck(64, 256, conv2_2)
+    conv2_1     = Bottleneck(64, 256, 'conv2_1', maxpool)
+    conv2_2     = Bottleneck(64, 256, 'conv2_2', conv2_1)
+    conv2_3     = Bottleneck(64, 256, 'conv2_3', conv2_2)
 
     convA_1     = Conv3x3(256, (2, 2), 'convA_1', conv2_3)
     convA_2     = Conv1x1(512, (1, 1), 'convA_2', convA_1)
 
-    conv3_1     = Bottleneck(128, 512, conv2_3, True)
-    conv3_2     = Bottleneck(128, 512, conv3_1)
-    conv3_3     = Bottleneck(128, 512, conv3_2)
-    conv3_4     = Bottleneck(128, 512, conv3_3)
+    conv3_1     = Bottleneck(128, 512, 'conv3_1', conv2_3, True)
+    conv3_2     = Bottleneck(128, 512, 'conv3_2', conv3_1)
+    conv3_3     = Bottleneck(128, 512, 'conv3_3', conv3_2)
+    conv3_4     = Bottleneck(128, 512, 'conv3_4', conv3_3)
 
     B_0         = Add()([conv3_4, convA_2])
     convB_1     = Conv3x3(512, (2, 2), 'convB_1', B_0)
     convB_2     = Conv1x1(1024, (1, 1), 'convB_2', convB_1)
 
-    conv4_1     = Bottleneck(256, 1024, conv3_4, True)
-    conv4_2     = Bottleneck(256, 1024, conv4_1)
-    conv4_3     = Bottleneck(256, 1024, conv4_2)
-    conv4_4     = Bottleneck(256, 1024, conv4_3)
-    conv4_5     = Bottleneck(256, 1024, conv4_4)
-    conv4_6     = Bottleneck(256, 1024, conv4_5)
-    conv4_7     = Bottleneck(256, 1024, conv4_6)
-    conv4_8     = Bottleneck(256, 1024, conv4_7)
-    conv4_9     = Bottleneck(256, 1024, conv4_8)
-    conv4_10    = Bottleneck(256, 1024, conv4_9)
-    conv4_11    = Bottleneck(256, 1024, conv4_10)
-    conv4_12    = Bottleneck(256, 1024, conv4_11)
-    conv4_13    = Bottleneck(256, 1024, conv4_12)
-    conv4_14    = Bottleneck(256, 1024, conv4_13)
-    conv4_15    = Bottleneck(256, 1024, conv4_14)
-    conv4_16    = Bottleneck(256, 1024, conv4_15)
-    conv4_17    = Bottleneck(256, 1024, conv4_16)
-    conv4_18    = Bottleneck(256, 1024, conv4_17)
-    conv4_19    = Bottleneck(256, 1024, conv4_18)
-    conv4_20    = Bottleneck(256, 1024, conv4_19)
-    conv4_21    = Bottleneck(256, 1024, conv4_20)
-    conv4_22    = Bottleneck(256, 1024, conv4_21)
-    conv4_23    = Bottleneck(256, 1024, conv4_22)
+    conv4_1     = Bottleneck(256, 1024, 'conv4_1', conv3_4, True)
+    conv4_2     = Bottleneck(256, 1024, 'conv4_2', conv4_1)
+    conv4_3     = Bottleneck(256, 1024, 'conv4_3', conv4_2)
+    conv4_4     = Bottleneck(256, 1024, 'conv4_4', conv4_3)
+    conv4_5     = Bottleneck(256, 1024, 'conv4_5', conv4_4)
+    conv4_6     = Bottleneck(256, 1024, 'conv4_6', conv4_5)
+    conv4_7     = Bottleneck(256, 1024, 'conv4_7', conv4_6)
+    conv4_8     = Bottleneck(256, 1024, 'conv4_8', conv4_7)
+    conv4_9     = Bottleneck(256, 1024, 'conv4_9', conv4_8)
+    conv4_10    = Bottleneck(256, 1024, 'conv4_10', conv4_9)
+    conv4_11    = Bottleneck(256, 1024, 'conv4_11', conv4_10)
+    conv4_12    = Bottleneck(256, 1024, 'conv4_12', conv4_11)
+    conv4_13    = Bottleneck(256, 1024, 'conv4_13', conv4_12)
+    conv4_14    = Bottleneck(256, 1024, 'conv4_14', conv4_13)
+    conv4_15    = Bottleneck(256, 1024, 'conv4_15', conv4_14)
+    conv4_16    = Bottleneck(256, 1024, 'conv4_16', conv4_15)
+    conv4_17    = Bottleneck(256, 1024, 'conv4_17', conv4_16)
+    conv4_18    = Bottleneck(256, 1024, 'conv4_18', conv4_17)
+    conv4_19    = Bottleneck(256, 1024, 'conv4_19',  conv4_18)
+    conv4_20    = Bottleneck(256, 1024, 'conv4_20', conv4_19)
+    conv4_21    = Bottleneck(256, 1024, 'conv4_21', conv4_20)
+    conv4_22    = Bottleneck(256, 1024, 'conv4_22', conv4_21)
+    conv4_23    = Bottleneck(256, 1024, 'conv4_23', conv4_22)
 
     C_0         = Add()([conv4_23, convB_2])
     convC_1     = Conv3x3(1024, (2, 2), 'convC_1', C_0)
     convC_2     = Conv1x1(2048, (1, 1), 'convC_2', convC_1)
 
-    conv5_1     = Bottleneck(512, 2048, conv4_23, True)
-    conv5_2     = Bottleneck(512, 2048, conv5_1)
-    conv5_3     = Bottleneck(512, 2048, conv5_2)
+    conv5_1     = Bottleneck(512, 2048, 'conv5_1', conv4_23, True)
+    conv5_2     = Bottleneck(512, 2048, 'conv5_2', conv5_1)
+    conv5_3     = Bottleneck(512, 2048, 'conv5_3', conv5_2)
 
     D_0         = Add()([conv5_3, convC_2])
-    avgpool     = AveragePooling2D(pool_size=(1, 1), strides=(1, 1), padding='valid')(D_0)
+    avgpool     = AveragePooling2D(pool_size=(1, 1), strides=(1, 1), padding='same')(D_0)
     flatten     = Flatten(name='flatten')(avgpool)
 
-    fc_detect   = Dense(512, activation='relu', name='fc_detect')(Flatten)
+    fc_detect   = Dense(512, activation='relu', name='fc_detect')(flatten)
     #fc_landmks  = Dense(512, activation = 'relu', name = 'fc_landmks')(flatten)
     #fc_vis      = Dense(512, activation = 'relu', name = 'fc_vis')(flatten)
     #fc_pose     = Dense(512, activation = 'relu', name = 'fc_pose')(flatten)
@@ -113,13 +117,14 @@ def ResNet101(input_shape):
     #out_pose    = Dense(3, activation = 'softmax', name = 'out_pose')(fc_pose)
     #out_gender  = Dense(2, activation = 'softmax', name = 'out_gender')(fc_gender)
 
-    model = Model(inputs = input_layer, outputs = [out_detect, out_landmks, out_vis, out_pose, out_gender])
+    model = Model(inputs = input_layer, outputs = out_detect)
+    #model = Model(inputs = input_layer, outputs = [out_detect, out_landmks, out_vis, out_pose, out_gender])
     return model
 
 def ResNet101HyperParameters(lr):
     optimizer = RMSprop(lr = lr, rho = 0.9, epsilon = None, decay = 0.0)
 
-    losses = {'out_detect': 'sparse_categorical_crossentropy'}
+    losses = {'out_detect': 'categorical_crossentropy'}
     '''losses = {
         "out_detect"    : "sparse_categorical_crossentropy",
         "out_landmks"   : "mean_squared_error",
@@ -162,8 +167,8 @@ def train(model, train_seq, validation_seq, args):
         verbose=1)
 
     # compile the model
-    optimizer, losses, loss_weights, reduce_lr = ResNet101HyperParameters(args.lr) 
-    model.compile(optimizer = optimizer, loss = losses, loss_weights = loss_weights, metrics = ['accuracy'], callbacks = [reduce_lr])
+    optimizer, losses, loss_weights, reduce_lr = ResNet101HyperParameters(args.lr)
+    model.compile(optimizer = optimizer, loss = losses, loss_weights = loss_weights, metrics = ['accuracy'])
 
     # Training without data augmentation:
     model.fit_generator(
@@ -227,8 +232,8 @@ if __name__ == "__main__":
         image_size=image_size)
 
     # define model
-    #model = multi_gpu_model(ResNet101(input_shape=image_size+(3,)), gpus=2)
-    model = ResNet101(input_shape=image_size+(3,))
+    model = multi_gpu_model(ResNet101(input_shape=image_size+(3,)), gpus=2)
+    #model = ResNet101(input_shape=image_size+(3,))
     model.summary()
 
     if args.weights is not None:
